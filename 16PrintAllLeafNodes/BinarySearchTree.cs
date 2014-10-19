fe87e94f5b272ea41b27f31d8b2b71d3f69f9479
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 12:00:31 -0700

    Prints all leaf nodes of a binary search tee
    Console-Implementation

---
+++ 16PrintAllLeafNodes/BinarySearchTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _16PrintAllLeafNodes
+{
+    //class BST
+    //{
+    // Creating Binary Search Tree node class
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+        public Node(int value)
+        {
+            this.data = value;
+            left = null;
+            right = null;
+        }
+    }
+    public class BinarySearchTree
+    {
+        public Node root;
+        static int count;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        //Creates and returns a BST node
+        public Node addNode(int data)
+        {
+            Node temp = new Node(data);
+            if (root == null)
+            {
+                root = temp;
+            }
+            count++;
+            return temp;
+        }
+
+        //Procedure inserts 'newNode' in BST at proper position
+        public void insert(Node root, Node newNode)
+        {
+            while (root != null)
+            {
+                if (newNode.data > root.data)
+                {
+                    if (root.right == null)
+                    {
+                        root.right = newNode;
+                        break;
+                    }
+                    root = root.right;
+                }
+                else
+                {
+                    if (root.left == null)
+                    {
+                        root.left = newNode;
+                        break;
+                    }
+                    root = root.left;
+                }
+            }
+        }            // Recursive Procedure travels BST tree Inorder Fassion
+        public void inOrder(Node root)
+        {
+            //Node temp = root;
+            //if (temp == null)
+            //{
+            //    return;
+            //}
+            //else 
+            if (root != null)
+            {
+                inOrder(root.left);
+                Console.Write(root.data + " ");
+                inOrder(root.right);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void printLeaves(Node temp)
+        {
+            if (temp == null)
+            {
+                return;
+            }
+            if (temp.left == null && temp.right == null)
+            {
+                System.Console.Write(temp.data + " ");
+            }
+            else
+            {
+                printLeaves(temp.left);
+                printLeaves(temp.right);
+            }
+        }
+    }
+    //}
+}
---
+++ 16PrintAllLeafNodes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _16PrintAllLeafNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            
+            // Inserting Nodes to Binary search Tree
+           
+            bst.insert(bst.root, bst.addNode(21));
+            bst.insert(bst.root, bst.addNode(19));
+            bst.insert(bst.root, bst.addNode(7));
+            bst.insert(bst.root, bst.addNode(18));
+            bst.insert(bst.root, bst.addNode(20));
+            bst.insert(bst.root, bst.addNode(13));
+            bst.insert(bst.root, bst.addNode(8));
+            bst.insert(bst.root, bst.addNode(22));
+
+            // Traversing BST in Inorder fashion
+            bst.inOrder(bst.root);
+            System.Console.WriteLine();
+            bst.printLeaves(bst.root);
+
+            Console.ReadKey();
+        }
+    }
+}
